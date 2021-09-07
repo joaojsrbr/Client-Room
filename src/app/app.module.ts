e83cadc852da7a4ddd7465cc7459e3454517841e
João Vitor <joaovitor.jsr@gmail.com>
2021-09-07 20:51:29 -0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    UpdateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    RoomListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/room.ts
@@ -0,0 +1,13 @@
+export class Room{
+    id:number;
+
+    name: string;
+
+    date:string;
+
+    startHour: string;
+
+    endHour: string;
+
+    active: boolean;
+}
